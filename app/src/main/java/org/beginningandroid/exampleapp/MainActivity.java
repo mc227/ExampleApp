c26283033320d6b11bc481194ced8415ab4044f1
mc227 <markcostales.mark@gmail.com>
2023-01-31 22:11:24 -0500

    created button and added log when it is clicked

--- app/src/main/java/org/beginningandroid/exampleapp/MainActivity.java
+++ app/src/main/java/org/beginningandroid/exampleapp/MainActivity.java
@@ -3,8 +3,16 @@ package org.beginningandroid.exampleapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    /** Called when the user touches the button */
+    public void sendMessage(View view) {
+        // Do something in response to button click
+        Log.i("INFO","Button Pressed");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
